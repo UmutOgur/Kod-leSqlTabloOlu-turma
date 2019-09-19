c10e9c5af495dd32acd7ce5115503f9e3e4f26e1
umut oğur <baxan_umut@hotmail.com>
2019-09-20 01:30:57 +0300

    first commit

---
+++ KodileSqlTabloOlusturma/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace KodileSqlTabloOlusturma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtsütun2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtsütun1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtveri1 = new System.Windows.Forms.TextBox();
+            this.txtveri2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tablo Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tablo Adı:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(105, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // txtsütun2
+            // 
+            this.txtsütun2.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsütun2.Location = new System.Drawing.Point(258, 97);
+            this.txtsütun2.Name = "txtsütun2";
+            this.txtsütun2.Size = new System.Drawing.Size(100, 21);
+            this.txtsütun2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(404, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Veri Tipi:";
+            // 
+            // txtsütun1
+            // 
+            this.txtsütun1.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsütun1.Location = new System.Drawing.Point(258, 61);
+            this.txtsütun1.Name = "txtsütun1";
+            this.txtsütun1.Size = new System.Drawing.Size(100, 21);
+            this.txtsütun1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(265, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sütun Adı:";
+            // 
+            // txtveri1
+            // 
+            this.txtveri1.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtveri1.Location = new System.Drawing.Point(399, 61);
+            this.txtveri1.Name = "txtveri1";
+            this.txtveri1.Size = new System.Drawing.Size(100, 21);
+            this.txtveri1.TabIndex = 8;
+            // 
+            // txtveri2
+            // 
+            this.txtveri2.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtveri2.Location = new System.Drawing.Point(399, 97);
+            this.txtveri2.Name = "txtveri2";
+            this.txtveri2.Size = new System.Drawing.Size(100, 21);
+            this.txtveri2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 299);
+            this.Controls.Add(this.txtveri2);
+            this.Controls.Add(this.txtveri1);
+            this.Controls.Add(this.txtsütun1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtsütun2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtsütun2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtsütun1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtveri1;
+        private System.Windows.Forms.TextBox txtveri2;
+    }
+}
+
---
+++ KodileSqlTabloOlusturma/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace KodileSqlTabloOlusturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-T2GKN5C\SQLEXPRESS;Initial Catalog=Dbo_FilmArsiv;Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string s1, s2, v1, v2;
+            //s1 = txtsütun1.Text;
+            //s2 = txtsütun2.Text;
+            //v1 = txtveri1.Text;
+            //v2 = txtveri2.Text;
+            try
+            {
+                baglanti.Open();
+                SqlCommand komuttablo = new SqlCommand("CREATE TABLE " + textBox1.Text.ToString() + "(" + txtsütun1.Text.ToString() + " " + txtveri1.Text.ToString() + "," + txtsütun2.Text.ToString() + " " + txtveri2.Text.ToString() + ")", baglanti);
+                komuttablo.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Tablo Oluşturuldu");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Tablo Oluşturulmadı");
+            }
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
